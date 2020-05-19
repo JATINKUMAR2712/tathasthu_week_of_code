315c8d0238595d5190c863d5b16287c6a020ebd9
JATINKUMAR2712 <65618415+JATINKUMAR2712@users.noreply.github.com>
2020-05-20 00:14:19 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,8 @@
+
+  Cost_Price = float(input("ENTER COST PRICE OF PRODUCT:"))
+  SELLING_Price = float(input("ENTER SELLING PRICE OF PRODUCT:"))
+  profit = SELLING_Price - Cost_Price
+  New_Selling_Price = 1.05 * profit + Cost_Price
+  print("Profit from this sell is ",profit)
+  print("to increase the profit by 5% selling price should be ",New_Selling_Price) 
+  
